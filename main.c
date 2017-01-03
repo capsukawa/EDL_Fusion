579f842c9755c6fb93c6fda6dfbabb43f85b5d35
Alexandre Cleyet-Merle <cleyetma@F201-10.e-im2ag.ujf-grenoble.fr>
2017-01-03 16:01:16 +0100

    blblblblblblblblblblblblbbl

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <elf.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "util.h"
 
 void reverseBytes(Elf32_Ehdr* header) {
-		
+	header->e_type = reverse_2(header->e_type);
+	header->e_machine = reverse_2(header->e_machine);
+	header->e_version = reverse_4(header->e_version);
+	header->e_entry = reverse_4(header->e_entry);
+	header->e_phoff = reverse_4(header->e_phoff);
+	header->e_shoff = reverse_4(header->e_shoff);
+	header->e_flags = reverse_4(header->e_flags);
+	header->e_ehsize = reverse_2(header->e_ehsize);
+	header->e_phentsize = reverse_2(header->e_phentsize);
+	header->e_phnum = reverse_2(header->e_phnum);
+	header->e_shentsize = reverse_2(header->e_shentsize);
+	header->e_shnum = reverse_2(header->e_shnum);
+	header->e_shstrndx = reverse_2(header->e_shstrndx);
 }
 
 Elf32_Ehdr* initHeader(char* fadr) {
