2c43980dcf3d8034ae43a40b63fd9ac50eaa4ee4
Melanie Carriere <carrieme@F201-15.e-im2ag.ujf-grenoble.fr>
2017-01-04 16:45:28 +0100

    Modification de contenuSection.c (sans les noms).

--- contenuSection.c
+++ contenuSection.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "header.h"
 #include "afficheHeader.h"
+#include "header.h"
+#include "util.h"
 
 
 void affichageSection(char* fadr, int num) {
@@ -16,33 +17,30 @@ void affichageSection(char* fadr, int num) {
 	header = initHeader(fadr);
 
 	int nbSection = header->e_shnum;	// Nombre de section du header
-	//int lsb = header->e_ident[5];	// little or big endian
 
-	if (num<=nbSection) {
+	if (num<nbSection) {
 		f = fopen(fadr,"r");
-		avancement = (header->e_shoff) + 40*(num-1) + 32 ;	
+
+		avancement = (header->e_shoff) + 40*(num) + 16 ;
 		// On va jusqu'a l'offset de la section demandee 
 		fseek(f,avancement,SEEK_SET);
 
 		fread(&adresse,4,1,f); 	// sauvegarde de l'adresse de la section
 		fread(&taille,4,1,f);	// sauvegarde de la taille de la section
 
-		char var[taille];
-			printf("\n%lx",avancement);
-			printf("\n%x",taille);
-			printf("\n%x\n",adresse);
+		if (header->e_ident[5]==2) {	// traitement du big endian 
+			taille = reverse_4(taille);
+			adresse = reverse_4(adresse);
+		}
+
+		unsigned char var[taille];
 
 		fseek(f,adresse,SEEK_SET);	// On va au debut du contenu de la section
 
 		fread(&var, 1, taille, f);
-		for (int i=0; i < taille; i++) {
-			printf("%x",var[i]);
 
+		for (int i=0; i < taille; i++) { // affichage 
+			printf("%x  ",var[i]);
 		}
-		
-		
-		//fwrite(f,1,taille,stdout); // affichage moche
-
 	}
-		printf("fin");
 }
