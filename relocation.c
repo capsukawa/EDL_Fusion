7fe6bf03052feed5a75475e7b8f7c4bbb4a78d15
Mehdi Baiyante <baiyantm@f213-14.e-im2ag.ujf-grenoble.fr>
2017-01-10 10:02:36 +0100

    segfalut reloc

--- relocation.c
+++ relocation.c
@@ -16,25 +16,26 @@ void initNbRelEntries(ElfFileStruct* elf) {
 
 void initRel(ElfFileStruct* elf, FILE* f) {
 	initNbRelEntries(elf);
-	int i, j;
-	int nbEntries; int tailleEntry;
+	int i, j, k=0;
+	int nbCurEntries; int tailleEntry;
 
 	elf->relTab = malloc(sizeof(Elf32_Rel)*elf->nbRel);
 	for (i=0;i<elf->header->e_shnum;i++) { //pour chaque section
 		//REL
 		if(elf->sections[i]->header->sh_type == SHT_REL) {
-			nbEntries = elf->sections[i]->header->sh_size/elf->sections[i]->header->sh_entsize;
+			nbCurEntries = elf->sections[i]->header->sh_size/elf->sections[i]->header->sh_entsize;
 			tailleEntry = sizeof(Elf32_Rel);
-			for (j=0;j<nbEntries;j++) { //pour chaque entrée dans la table de relocation
+			for (j=0;j<nbCurEntries;j++) { //pour chaque entrée dans la table de relocation
 				fseek(f,elf->sections[i]->header->sh_offset+j*tailleEntry,SEEK_SET); //seek au bon endroit
-				elf->relTab[j]=malloc(sizeof(Elf32_Rel)); //allocation memoire
-				fread(elf->relTab[j], 1, tailleEntry, f); //lecture dans le fichier et affectation dans la structure
+				elf->relTab[k]=malloc(sizeof(Elf32_Rel)); //allocation memoire
+				fread(elf->relTab[k], 1, tailleEntry, f); //lecture dans le fichier et affectation dans la structure
 
 				//endianess
 				if (elf->header->e_ident[5]==2) {
-					elf->relTab[j]->r_offset = reverse_4(elf->relTab[j]->r_offset);
-					elf->relTab[j]->r_info = reverse_4(elf->relTab[j]->r_info);
+					elf->relTab[k]->r_offset = reverse_4(elf->relTab[k]->r_offset);
+					elf->relTab[k]->r_info = reverse_4(elf->relTab[k]->r_info);
 				}
+				k++;
 			}
 		}
 	}
